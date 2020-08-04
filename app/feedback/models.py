f6439e59ba91fedd6ab8b8ed927dd9083031b121
chiaki <chiaki.aws@gmail.com>
2020-08-04 01:49:22 -0600

    feedbackのデータ形式をTextAreaからCharFieldに変更

--- app/feedback/models.py
+++ app/feedback/models.py
@@ -8,9 +8,8 @@ from django.contrib.auth.models import User
 class Feedback(models.Model):
 
     LEVEL_CHOICES = ((5,"Satisfecho"), (4,"Bueno"), (3, "Ordinario"), (3, "Insatisfecho"), (1, "Malo"),)
-
     evaluator = models.ForeignKey(User, on_delete=models.PROTECT)
-    content   = models.TextField()
+    content   = models.CharField(max_length=50, null=True, blank=True)
     level     = models.IntegerField(choices=LEVEL_CHOICES)
 
     def __str__(self): 
